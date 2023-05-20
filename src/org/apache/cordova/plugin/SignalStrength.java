fd11e15796fc999c16223f89b92ea351f557fffb
henrique-gerhardt <henrique.gerhardt@hotmail.com>
2023-05-20 08:04:48 -0300

    Completly rewritten plugin to support Android 4.2 and above.

--- src/org/apache/cordova/plugin/SignalStrength.java
+++ src/org/apache/cordova/plugin/SignalStrength.java
@@ -1,63 +1,117 @@
 package org.apache.cordova.plugin;
 
 import android.content.Context;
-import android.os.Build;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoCdma;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
 import android.telephony.CellSignalStrength;
+import android.telephony.CellSignalStrengthCdma;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthLte;
+import android.telephony.CellSignalStrengthWcdma;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-
-import org.apache.cordova.CordovaPlugin;
+import java.lang.Exception;
+import java.lang.Thread;
+import java.util.List;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PermissionHelper;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class SignalStrength extends CordovaPlugin {
 
-@Override
-public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        private TelephonyManager tm;
+        private int asulevel = -1;
+        private int dbm = -1;
 
-        if (action.equals("dbm")) {
-                ssListener = new SignalStrengthStateListener();
-                TelephonyManager tm = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-                tm.listen(ssListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
-                int counter = 0;
-                while ( dbm == -1) {
-                        try {
-                                Thread.sleep(200);
-                        } catch (InterruptedException e) {
-                                e.printStackTrace();
+        @Override
+        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+                if (action.equals("dbm")) {
+                        ssListener = new SignalStrengthStateListener();
+                        tm = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+                        
+                        if(!PermissionHelper.hasPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+                                PermissionHelper.requestPermission(this, CONTINUE, Manifest.permission.READ_PHONE_STATE);
+                        } else {
+                                this.dbm = getDbm();
                         }
-                        if (counter++ >= 5)
-                        {
-                                break;
-                        }
+                        callbackContext.success(this.dbm);
+                        return true;
                 }
-                callbackContext.success(dbm);
-                return true;
+
+                return false;
         }
 
-        return false;
-}
+        public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+                for (int r : grantResults) {
+                        if (r == PackageManager.PERMISSION_DENIED) {
+                                this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, PERMISSION_DENIED_ERROR));
+                                return;
+                        }
+                }
+                switch (requestCode) {
+                        case CONTINUE:
+                                this.dbm = getDbm();
+                                break;
+                }
+        }
 
+        private int getDbm() {
+                List<CellInfo> cellInfoList = this.tm.getAllCellInfo();
+                if (cellInfoList != null) {
+                        for (CellInfo cellInfo : cellInfoList) {
+                                if (cellInfo instanceof CellInfoLte) {
+                                        CellSignalStrengthLte cellSignalStrengthLte = cellInfo.getCellSignalStrength();
+                                        return cellSignalStrengthLte.getDbm();
+                                } else if (cellInfo instanceof CellInfoWcdma) {
+                                        CellSignalStrengthWcdma cellSignalStrengthWcdma = cellInfo.getCellSignalStrength();
+                                        return cellSignalStrengthWcdma.getDbm();
+                                } else if (cellInfo instanceof CellInfoGsm) {
+                                        CellSignalStrengthGsm cellSignalStrengthGsm = cellInfo.getCellSignalStrength();
+                                        return cellSignalStrengthGsm.getDbm();
+                                } else if (cellInfo instanceof CellInfoCdma) {
+                                        CellSignalStrengthCdma cellSignalStrengthCdma = cellInfo.getCellSignalStrength();
+                                        return cellSignalStrengthCdma.getDbm();
+                                }
+                        }
+                } else {
+                        try { 
+                                SignalStrengthStateListener sssListener = new SignalStrengthStateListener();
+                                this.tm.listen(sssListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+                                int cc = 0;
+                                while (this.asulevel == -1) {
+                                        Thread.sleep(300);
+                                        if (cc++ >= 5)
+                                        {
+                                                break;
+                                        }
+                                }
+                                this.tm.listen(sssListener, PhoneStateListener.LISTEN_NONE);
+                                
+                                if(this.asulevel == -1) {
+                                        return -1;
+                                }
 
-class SignalStrengthStateListener extends PhoneStateListener {
+                                return -113 + 2 * this.asulevel;
+                        } catch (Exception e) {
+                                throw new IllegalArgumentException(e);
+                        }
+                }
+        }
 
-@Override
-public void onSignalStrengthsChanged(android.telephony.SignalStrength signalStrength) {
-        super.onSignalStrengthsChanged(signalStrength);
-
-        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        CellSignalStrength cellSignalStrength = signalStrength.getCellSignalStrengths().get(0);
-        dbm = cellSignalStrength.getDbm();
-        /*} else {
-                int tsNormSignalStrength = signalStrength.getGsmSignalStrength();
-                dbm = (2 * tsNormSignalStrength) - 113;     // -> dBm
-        }*/
-}
-
-}
-
-SignalStrengthStateListener ssListener;
-int dbm = -1;
-
+        public class SignalStrengthStateListener extends PhoneStateListener {
+                @Override
+                public void onSignalStrengthsChanged(android.telephony.SignalStrength signalStrength) {
+                        super.onSignalStrengthsChanged(signalStrength);
+                        this.asulevel = signalStrength.getGsmSignalStrength();
+                }
+        }
 }
